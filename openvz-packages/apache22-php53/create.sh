9376056971c29c2e13c77c86d6fe3c17cdf1e8bd
Donatas Abraitis <donatas@vinted.com>
2016-01-14 12:14:24 +0200

    Destroy container before creating the new one

--- openvz-packages/apache22-php53/create.sh
+++ openvz-packages/apache22-php53/create.sh
@@ -1,3 +1,5 @@
+vzctl umount 53
+vzctl stop 53
 vzctl destroy 53 || exit 1
 vzctl create 53 --ostemplate centos-6-x86_64 --ipadd 192.168.0.53 || exit 1
 vzctl start 53 || exit 1
@@ -18,4 +20,3 @@ vzctl exec 53 rm -rf /root/mod_rpaf || exit 1
 vzctl stop 53 || exit 1
 vzctl mount 53 || exit 1
 cd /vz/root/53 && tar --numeric-owner -czf /root/apache22-php53-$(date +%s).tar.gz . || exit 1
-cd ; vzctl umount 53 || exit 1
