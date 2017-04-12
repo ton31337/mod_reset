d423c8c9dee61531073d06739c3c12cba4d1b5e6
Donatas Abraitis <donatas.abraitis@gmail.com>
2017-04-12 14:44:03 +0300

    Add arbitrary header support

--- mod_reset.c
+++ mod_reset.c
@@ -15,11 +15,24 @@ static int reset_handler(request_rec *r)
         core_server_config *core = ap_get_module_config(r->server->module_config, &core_module);
 #endif
         if (conf->enable) {
+                if (conf->deny_header) {
+                        char *proxy_ts = (char *) apr_table_get(r->headers_in, conf->deny_header);
+
+                        /* Check if request contains arbitrary header set by proxy */
+                        if (!proxy_ts)
+                               return HTTP_FORBIDDEN;
+
+                        apr_time_t current_ts = apr_time_now();
+
+                        /* Check if current TS is valid */
+                        if (apr_time_sec(current_ts) < atoi(proxy_ts))
+                                return HTTP_FORBIDDEN;
+                }
 #ifndef NO_PHP
                 apr_array_header_t *arr = (apr_array_header_t *) apr_table_elts(conf->php_ini);
                 apr_table_entry_t *ini = (apr_table_entry_t *) arr->elts;
 
-                // Setting php.ini values on the fly
+                /* Setting php.ini values on the fly */
                 int i;
                 for (i = 0; i < arr->nelts; i++) {
                         char *value = (char *) apr_table_get(r->headers_in, ini[i].val);
@@ -46,7 +59,7 @@ static int reset_handler(request_rec *r)
 #endif
 #endif
 
-                // Setting DocumentRoot
+                /* Setting DocumentRoot */
                 char *docroot = (char *) apr_table_get(r->headers_in, conf->docroot);
                 if (docroot && ap_is_directory(r->pool, docroot)) {
 #ifdef APACHE_22
@@ -62,7 +75,7 @@ static int reset_handler(request_rec *r)
                 }
 
 #ifndef NO_PHP
-               // Set TMPDIR environment variable
+               /* Set TMPDIR environment variable */
                char *tmpdir = (char *) apr_table_get(r->headers_in, conf->tmpdir);
                if (tmpdir && ap_is_directory(r->pool, tmpdir)) {
                    if (apr_env_set("TMPDIR", tmpdir, r->pool) != APR_SUCCESS) {
@@ -73,7 +86,7 @@ static int reset_handler(request_rec *r)
                }
 #endif
 
-                // Setting ServerAdmin
+                /* Setting ServerAdmin */
                 char *admin = (char *) apr_table_get(r->headers_in, conf->admin);
                 if (admin) {
                         apr_table_setn(r->subprocess_env, "SERVER_ADMIN", admin);
@@ -95,6 +108,14 @@ static const char *enable_reset(cmd_parms *cmd, void *cfg, const char arg[])
                 conf->enable = 1;
                 conf->php_ini = apr_table_make(cmd->pool, conf->nheaders);
         }
+        return NULL;
+}
+
+static const char *denyheader_reset(cmd_parms *cmd, void *cfg, const char arg[])
+{
+        reset_config *conf = (reset_config *) ap_get_module_config(cmd->server->module_config, &reset_module);
+        if (arg != NULL && strlen(arg) > 0)
+                conf->deny_header = arg;
         return NULL;
 }
 
@@ -141,6 +162,7 @@ static void * reset_create_server_config(apr_pool_t * p, server_rec * s) {
 static const command_rec reset_module_directives[] =
 {
         AP_INIT_TAKE1("Reset", enable_reset, NULL, RSRC_CONF, "Enable/Disable reset module."),
+        AP_INIT_TAKE1("ResetDenyHeader", denyheader_reset, NULL, RSRC_CONF, "Set header which is used to deny unauthenticated requests."),
         AP_INIT_TAKE2("ResetHeaders", headers_reset, NULL, RSRC_CONF, "Configure headers to be used for checking."),
         AP_INIT_TAKE2("ResetHeader", header_reset, NULL, RSRC_CONF, "Configure header for setting ServerAdmin/DocumentRoot."),
         {NULL}
